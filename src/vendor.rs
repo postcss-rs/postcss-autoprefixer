802d05b8e8b7bbb827b0aa03baf24d02c8abfdf7
IWANABETHATGUY <974153916@qq.com>
2021-11-26 21:53:25 +0800

    feat: 🎸 init

---
+++ src/vendor.rs
@@ -0,0 +1,22 @@
+
+// use recursive_parser::parser::Prop;
+// module.exports = {
+//   prefix(prop) {
+//     let match = prop.match(/^(-\w+-)/)
+//     if (match) {
+//       return match[0]
+//     }
+
+//     return ''
+//   },
+
+//   unprefixed(prop) {
+//     return prop.replace(/^-\w+-/, '')
+//   }
+// }
+
+pub fn get_prefix() {}
+
+pub fn remove_prefix(prop: &str) {
+
+}
